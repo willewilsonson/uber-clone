eb5ad8860dce13c9c575c572e8310d4884ac8f3e
willewilsonson <william.nordenstad@gmail.com>
2021-11-02 11:07:55 +0100

    added homescreen, icons, navigation, env

--- App.js
+++ App.js
@@ -2,18 +2,41 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { Provider } from 'react-redux';
+import HomeScreen from './screens/HomeScreen';
+import MapScreen from './screens/MapScreen';
 import { store } from './store';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
 export default function App() {
+  const Stack = createStackNavigator();
+
   return (
     <Provider store={store}>
-      <View style={styles.container}>
-        <Text>Let's build Uber!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <SafeAreaProvider>
+          <Stack.Navigator>
+            <Stack.Screen 
+              name='HomeScreen'
+              component={HomeScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+            <Stack.Screen 
+              name='MapScreen'
+              component={MapScreen}
+              options={{
+                headerShown: false,
+              }}
+            />
+          </Stack.Navigator>
+        </SafeAreaProvider>
+      </NavigationContainer>
     </Provider>
   );
-}
+};
 
 const styles = StyleSheet.create({
   container: {
